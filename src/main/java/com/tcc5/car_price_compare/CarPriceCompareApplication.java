aaf4a64d2f4ecf03164732a9de23e2cb55d194c3
Paulo Henrique <ph.1928@hotmail.com>
2024-08-30 20:49:50 -0300

    First commit

---
+++ src/main/java/com/tcc5/car_price_compare/CarPriceCompareApplication.java
@@ -0,0 +1,13 @@
+package com.tcc5.car_price_compare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPriceCompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPriceCompareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc5/car_price_compare/CarPriceCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc5.car_price_compare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPriceCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
