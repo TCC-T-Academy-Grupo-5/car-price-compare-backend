a7007a0aab8a2fbb3fb90d2c057e3dafc9646e5d
kingabreel <gabrielyury441@gmail.com>
2024-09-23 13:26:00 -0300

    feat: DTOs for vehicle and price

---
+++ src/main/java/com/tcc5/car_price_compare/domain/vehicle/dto/BrandDTO.java
@@ -0,0 +1,4 @@
+package com.tcc5.car_price_compare.domain.vehicle.dto;
+
+public record BrandDTO(String brand, String code) {
+}
---
+++ src/main/java/com/tcc5/car_price_compare/domain/vehicle/dto/VehicleDTO.java
@@ -0,0 +1,4 @@
+package com.tcc5.car_price_compare.domain.vehicle.dto;
+
+public record VehicleDTO (String fipeCode, String referenceMonth, String fuelType, String releaseYearId){
+}
