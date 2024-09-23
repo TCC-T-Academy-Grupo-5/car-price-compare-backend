19907414e958c36bc8605197475812a2c091a5f8
Paulo Henrique <ph.1928@hotmail.com>
2024-09-23 13:50:42 -0300

    feat: add Model and ModelRepository

---
+++ src/main/java/com/tcc5/car_price_compare/domain/vehicle/Model.java
@@ -0,0 +1,40 @@
+package com.tcc5.car_price_compare.domain.vehicle;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.tcc5.car_price_compare.domain.shared.GenericTimestamp;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "model")
+public class Model extends GenericTimestamp {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String urlPathName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "brand_id")
+    @JsonBackReference
+    private Brand brand;
+
+    @OneToMany(mappedBy = "model", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Year> years = new ArrayList<>();
+}
