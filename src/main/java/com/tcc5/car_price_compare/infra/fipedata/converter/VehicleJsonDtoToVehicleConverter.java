884b7f30252913bb734a61268de6055846a5a34e
Paulo Henrique <ph.1928@hotmail.com>
2024-09-23 17:19:48 -0300

    feat: VehicleJsonDto and VehicleJsonDtoToModelConverter

---
+++ src/main/java/com/tcc5/car_price_compare/infra/fipedata/converter/VehicleJsonDtoToVehicleConverter.java
@@ -0,0 +1,31 @@
+package com.tcc5.car_price_compare.infra.fipedata.converter;
+
+import com.tcc5.car_price_compare.domain.vehicle.Vehicle;
+import com.tcc5.car_price_compare.domain.vehicle.Year;
+import com.tcc5.car_price_compare.infra.fipedata.dto.VehicleJsonDto;
+import com.tcc5.car_price_compare.repositories.vehicle.YearRepository;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VehicleJsonDtoToVehicleConverter implements Converter<VehicleJsonDto, Vehicle> {
+
+    private final YearRepository yearRepository;
+
+    public VehicleJsonDtoToVehicleConverter(YearRepository yearRepository) {
+        this.yearRepository = yearRepository;
+    }
+
+    @Override
+    public Vehicle convert(VehicleJsonDto source) {
+        Year year = this.yearRepository.findById(source.year_id()).orElseThrow(() -> {
+            return new RuntimeException("Error converting VehicleJsonDto to Vehicle: no year with id " + source.year_id());
+        });
+
+        Vehicle vehicle = new Vehicle();
+        vehicle.setId(source.year_id());
+        vehicle.setFipeCode(source.fipe_code());
+        vehicle.setYear(year);
+        return vehicle;
+    }
+}
