af574d203d491dc5f3548e82caa74a54b7efbd53
Paulo Henrique <ph.1928@hotmail.com>
2024-09-23 14:24:55 -0300

    feat: add importBrands method to FipeDataImporter

--- src/main/java/com/tcc5/car_price_compare/infra/fipedata/FipeDataImporter.java
+++ src/main/java/com/tcc5/car_price_compare/infra/fipedata/FipeDataImporter.java
@@ -3,7 +3,9 @@ package com.tcc5.car_price_compare.infra.fipedata;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tcc5.car_price_compare.infra.fipedata.dto.BrandJsonDTO;
+import com.tcc5.car_price_compare.domain.vehicle.Brand;
+import com.tcc5.car_price_compare.infra.fipedata.converter.BrandJsonDtoToBrandConverter;
+import com.tcc5.car_price_compare.infra.fipedata.dto.BrandJsonDto;
 import com.tcc5.car_price_compare.repositories.vehicle.BrandRepository;
 import com.tcc5.car_price_compare.repositories.vehicle.ModelRepository;
 import com.tcc5.car_price_compare.repositories.vehicle.VehicleRepository;
@@ -16,19 +18,23 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class FipeDataImporter implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(FipeDataImporter.class);
-    private static final WebClient webClient = WebClient.builder().build();
+    private final Logger log = LoggerFactory.getLogger(FipeDataImporter.class);
+    private final WebClient webClient = WebClient.builder().build();
 
     private final BrandRepository brandRepository;
     private final ModelRepository modelRepository;
     private final YearRepository yearRepository;
     private final VehicleRepository vehicleRepository;
+
     private final ObjectMapper objectMapper;
+
+    private final BrandJsonDtoToBrandConverter brandJsonDtoToBrandConverter;
 
     @Value("${fipe.data.brands.url}")
     private String brandsUrl;
@@ -46,12 +52,14 @@ public class FipeDataImporter implements CommandLineRunner {
             ModelRepository modelRepository,
             YearRepository yearRepository,
             VehicleRepository vehicleRepository,
-            ObjectMapper objectMapper) {
+            ObjectMapper objectMapper,
+            BrandJsonDtoToBrandConverter brandJsonDtoToBrandConverter) {
         this.brandRepository = brandRepository;
         this.modelRepository = modelRepository;
         this.yearRepository = yearRepository;
         this.vehicleRepository = vehicleRepository;
         this.objectMapper = objectMapper;
+        this.brandJsonDtoToBrandConverter = brandJsonDtoToBrandConverter;
     }
 
     @Override
@@ -61,6 +69,10 @@ public class FipeDataImporter implements CommandLineRunner {
     }
 
     private void importBrands() {
+        if (this.brandRepository.count() == 0) {
+            return;
+        }
+
         this.webClient.get()
                 .uri(brandsUrl)
                 .retrieve()
@@ -68,11 +80,15 @@ public class FipeDataImporter implements CommandLineRunner {
                 .subscribe(
                         brands -> {
                             try {
-                                List<BrandJsonDTO> brandsFromJson = this.objectMapper.readValue(brands, new TypeReference<>() {});
-                                for (BrandJsonDTO brandJsonDTO : brandsFromJson) {
-                                    System.out.println(brandJsonDTO);
-                                }
+                                log.info("Importing brands...");
 
+                                List<BrandJsonDto> brandsFromJson = this.objectMapper.readValue(brands, new TypeReference<>() {});
+                                List<Brand> brandsToSave = brandsFromJson.stream()
+                                        .map(this.brandJsonDtoToBrandConverter::convert)
+                                        .toList();
+
+                                this.brandRepository.saveAll(brandsToSave);
+                                log.info("Brands imported to database successfully");
                             } catch (JsonProcessingException e) {
                                 log.error("Error importing brands: {}", e.getMessage());
                             }
