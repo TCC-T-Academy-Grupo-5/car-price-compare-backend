b747e2e837c547e1cc4902ec093ee5c731fba0b4
Paulo Henrique <ph.1928@hotmail.com>
2024-09-23 13:49:27 -0300

    feat: add Brand and BrandRepository

---
+++ src/main/java/com/tcc5/car_price_compare/domain/vehicle/Brand.java
@@ -0,0 +1,39 @@
+package com.tcc5.car_price_compare.domain.vehicle;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.tcc5.car_price_compare.domain.shared.GenericTimestamp;
+import com.tcc5.car_price_compare.domain.vehicle.enums.VehicleType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "brand")
+public class Brand extends GenericTimestamp {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String urlPathName;
+
+    @Enumerated(EnumType.ORDINAL)
+    private VehicleType vehicleType;
+
+    @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<Model> models = new ArrayList<>();
+}
