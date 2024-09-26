c5ce3058ae3ee0b511c46d19bcf5d92ac45a53fa
Paulo Henrique <ph.1928@hotmail.com>
2024-09-26 14:41:13 -0300

    chore: remove ResponseDto

--- src/main/java/com/tcc5/car_price_compare/domain/shared/ResponseDto.java
+++
@@ -1,6 +0,0 @@
-package com.tcc5.car_price_compare.domain.shared;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record ResponseDto(String error, @NotBlank String message, Object data) {
-}
