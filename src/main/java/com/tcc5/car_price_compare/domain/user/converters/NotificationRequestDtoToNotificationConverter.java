d6970589aa223d07c65dd6e32b39a3cbf5be56f9
Paulo Henrique <ph.1928@hotmail.com>
2024-09-26 14:42:04 -0300

    feat: add vehicle relation to dto converter

--- src/main/java/com/tcc5/car_price_compare/domain/user/converters/NotificationRequestDtoToNotificationConverter.java
+++ src/main/java/com/tcc5/car_price_compare/domain/user/converters/NotificationRequestDtoToNotificationConverter.java
@@ -3,20 +3,34 @@ package com.tcc5.car_price_compare.domain.user.converters;
 import com.tcc5.car_price_compare.domain.user.User;
 import com.tcc5.car_price_compare.domain.user.dto.NotificationRequestDTO;
 import com.tcc5.car_price_compare.domain.user.features.Notification;
+import com.tcc5.car_price_compare.domain.vehicle.Vehicle;
+import com.tcc5.car_price_compare.domain.vehicle.VehicleNotFoundException;
+import com.tcc5.car_price_compare.repositories.vehicle.VehicleRepository;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificationRequestDtoToNotificationConverter implements Converter<NotificationRequestDTO, Notification> {
+
+    private final VehicleRepository vehicleRepository;
+
+    public NotificationRequestDtoToNotificationConverter(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
     @Override
     public Notification convert(NotificationRequestDTO source) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Vehicle vehicle = this.vehicleRepository.findById(String.valueOf(source.vehicleId()))
+                .orElseThrow(() -> new VehicleNotFoundException(source.vehicleId()));
 
         Notification notification = new Notification();
         notification.setNotificationType(source.notificationType());
         notification.setCurrentFipePrice(source.currentFipePrice());
         notification.setUser(user);
+        notification.setVehicle(vehicle);
+
         return notification;
     }
 }
--- src/main/java/com/tcc5/car_price_compare/domain/user/dto/NotificationRequestDTO.java
+++ src/main/java/com/tcc5/car_price_compare/domain/user/dto/NotificationRequestDTO.java
@@ -3,8 +3,11 @@ package com.tcc5.car_price_compare.domain.user.dto;
 import com.tcc5.car_price_compare.domain.user.enums.NotificationType;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.UUID;
+
 public record NotificationRequestDTO(
         @NotNull NotificationType notificationType,
-        @NotNull Double currentFipePrice
-) {
+        @NotNull Double currentFipePrice,
+        @NotNull UUID vehicleId
+        ) {
 }
