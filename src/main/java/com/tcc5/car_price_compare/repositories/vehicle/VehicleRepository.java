4e446df3f4a137a28dbb879eb56714b1d623c9d4
Paulo Henrique <ph.1928@hotmail.com>
2024-09-27 10:10:06 -0300

    feat: add JPA queries for retrieving VehicleDTOs from the database

--- src/main/java/com/tcc5/car_price_compare/repositories/vehicle/VehicleRepository.java
+++ src/main/java/com/tcc5/car_price_compare/repositories/vehicle/VehicleRepository.java
@@ -1,9 +1,33 @@
 package com.tcc5.car_price_compare.repositories.vehicle;
 
 import com.tcc5.car_price_compare.domain.vehicle.Vehicle;
+import com.tcc5.car_price_compare.domain.vehicle.dto.VehicleDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
+
+    @Query(
+        "SELECT new com.tcc5.car_price_compare.domain.vehicle.dto.VehicleDTO(v.id, v.fipeCode, y.name, m.name, b.name, b.vehicleType) " +
+        "FROM Vehicle v " +
+        "JOIN v.year y " +
+        "JOIN y.model m " +
+        "JOIN m.brand b"
+    )
+    List<VehicleDTO> findAllVehicleDTOs();
+
+    @Query(
+        "SELECT new com.tcc5.car_price_compare.domain.vehicle.dto.VehicleDTO(v.id, v.fipeCode, y.name, m.name, b.name, b.vehicleType) " +
+        "FROM Vehicle v " +
+        "JOIN v.year y " +
+        "JOIN y.model m " +
+        "JOIN m.brand b " +
+        "WHERE v.id = :id"
+    )
+    Optional<VehicleDTO> findVehicleDTOById(@Param("id") UUID id);
 }
