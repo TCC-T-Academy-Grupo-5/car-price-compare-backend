e9c9e98d056cbe879793e644a7f0ea08d3810ed7
Paulo Henrique <ph.1928@hotmail.com>
2024-09-27 11:41:26 -0300

    chore: remove unused dependencies

--- src/main/java/com/tcc5/car_price_compare/domain/vehicle/converters/VehicleToVehicleDTOConverter.java
+++ src/main/java/com/tcc5/car_price_compare/domain/vehicle/converters/VehicleToVehicleDTOConverter.java
@@ -2,13 +2,10 @@ package com.tcc5.car_price_compare.domain.vehicle.converters;
 
 import com.tcc5.car_price_compare.domain.vehicle.Vehicle;
 import com.tcc5.car_price_compare.domain.vehicle.dto.VehicleDTO;
-import com.tcc5.car_price_compare.domain.vehicle.enums.VehicleType;
 import com.tcc5.car_price_compare.domain.vehicle.exceptions.VehicleNotFoundException;
 import com.tcc5.car_price_compare.repositories.vehicle.VehicleRepository;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Component
 public class VehicleToVehicleDTOConverter implements Converter<Vehicle, VehicleDTO> {
