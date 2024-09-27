df4ac47b763b9ca18076b033d4dde91f90461130
Paulo Henrique <ph.1928@hotmail.com>
2024-09-27 10:10:30 -0300

    refactor: change VehicleToVehicleDTOConverter to use the new JPA queries in vehicleRepository

--- src/main/java/com/tcc5/car_price_compare/domain/vehicle/converters/VehicleToVehicleDTOConverter.java
+++ src/main/java/com/tcc5/car_price_compare/domain/vehicle/converters/VehicleToVehicleDTOConverter.java
@@ -3,6 +3,8 @@ package com.tcc5.car_price_compare.domain.vehicle.converters;
 import com.tcc5.car_price_compare.domain.vehicle.Vehicle;
 import com.tcc5.car_price_compare.domain.vehicle.dto.VehicleDTO;
 import com.tcc5.car_price_compare.domain.vehicle.enums.VehicleType;
+import com.tcc5.car_price_compare.domain.vehicle.exceptions.VehicleNotFoundException;
+import com.tcc5.car_price_compare.repositories.vehicle.VehicleRepository;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
@@ -11,15 +13,15 @@ import java.util.UUID;
 @Component
 public class VehicleToVehicleDTOConverter implements Converter<Vehicle, VehicleDTO> {
 
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleToVehicleDTOConverter(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
     @Override
     public VehicleDTO convert(Vehicle source) {
-        UUID id = source.getId();
-        String fipeCode = source.getFipeCode();
-        String yearName = source.getYear().getName();
-        String modelName = source.getYear().getModel().getName();
-        String brandName = source.getYear().getModel().getBrand().getName();
-        VehicleType vehicleType = source.getYear().getModel().getBrand().getVehicleType();
-
-        return new VehicleDTO(id, fipeCode, yearName, modelName, brandName, vehicleType);
+        return this.vehicleRepository.findVehicleDTOById(source.getId())
+                .orElseThrow(() -> new VehicleNotFoundException(source.getId()));
     }
 }
