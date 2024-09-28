bd8a29a06990a466abff9fd12f819d85b61d6987
Paulo Henrique <ph.1928@hotmail.com>
2024-09-28 12:10:19 -0300

    refactor: add @Getter to TimestampedEntity

--- src/main/java/com/tcc5/car_price_compare/domain/shared/TimestampedEntity.java
+++ src/main/java/com/tcc5/car_price_compare/domain/shared/TimestampedEntity.java
@@ -1,12 +1,14 @@
 package com.tcc5.car_price_compare.domain.shared;
 
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@Getter
 public abstract class TimestampedEntity {
 
     @CreationTimestamp
