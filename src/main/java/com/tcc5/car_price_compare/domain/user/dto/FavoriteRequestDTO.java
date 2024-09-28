d03d3ae75ba566bb142961b7d00168ed9b2d25d9
Paulo Henrique <ph.1928@hotmail.com>
2024-09-28 16:22:08 -0300

    refactor: add validation to favorite request DTO

--- src/main/java/com/tcc5/car_price_compare/domain/user/dto/FavoriteRequestDTO.java
+++ src/main/java/com/tcc5/car_price_compare/domain/user/dto/FavoriteRequestDTO.java
@@ -1,6 +1,8 @@
 package com.tcc5.car_price_compare.domain.user.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
-public record FavoriteRequestDTO(UUID vehicleId) {
+public record FavoriteRequestDTO(@NotNull UUID vehicleId) {
 }
