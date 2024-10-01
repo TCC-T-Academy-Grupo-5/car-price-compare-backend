824f40f0ad4b39160150996acbdd842e303f5e41
kingabreel <gabrielyury441@gmail.com>
2024-10-01 20:34:18 -0300

    feat: endpoint to add brand, vehicle and model

--- src/main/java/com/tcc5/car_price_compare/controllers/VehicleController.java
+++ src/main/java/com/tcc5/car_price_compare/controllers/VehicleController.java
@@ -1,7 +1,13 @@
 package com.tcc5.car_price_compare.controllers;
 
 import com.tcc5.car_price_compare.domain.response.vehicle.VehicleResponse;
+import com.tcc5.car_price_compare.domain.vehicle.Brand;
+import com.tcc5.car_price_compare.domain.vehicle.Model;
+import com.tcc5.car_price_compare.domain.vehicle.dto.BrandDTO;
+import com.tcc5.car_price_compare.domain.vehicle.dto.ModelDTO;
+import com.tcc5.car_price_compare.domain.vehicle.dto.VehicleDTO;
 import com.tcc5.car_price_compare.services.VehicleService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -18,9 +24,31 @@ public class VehicleController {
     @GetMapping
     public ResponseEntity<Page<VehicleResponse>> getVehicles(
             @RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
-            @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize
-    ){
-        return ResponseEntity.status(HttpStatus.OK).body(service.getVehicles(pageNumber, pageSize));
+            @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
+            @RequestParam(value = "model", required = false) String model,
+            @RequestParam(value = "brand", required = false) String brand,
+            @RequestParam(value = "fipePrice", required = false) Double fipePrice,
+            @RequestParam(value = "type", required = false) Integer type,
+            @RequestParam(value = "year", required = false) String year
+            ){
+
+        return ResponseEntity.status(HttpStatus.OK).body(service.getVehicles(pageNumber, pageSize, model, brand, fipePrice, type, year));
+    }
+
+    @PostMapping()
+    public ResponseEntity<VehicleResponse> addVehicle(@RequestBody @Valid VehicleDTO vehicleDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.addVehicle(vehicleDTO));
+    }
+
+    @PostMapping("/brand")
+    public ResponseEntity<Brand> addBrand(@RequestBody @Valid BrandDTO brandDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.addBrand(brandDTO));
+
+    }
+
+    @PostMapping("/model")
+    public ResponseEntity<Model> addModel(@RequestBody @Valid ModelDTO modelDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.addModel(modelDTO));
     }
 }
 
