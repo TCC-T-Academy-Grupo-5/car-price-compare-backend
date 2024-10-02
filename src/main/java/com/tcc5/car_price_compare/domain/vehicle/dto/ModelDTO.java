e4df53021557322838e3cb87070863f1c76c6acc
kingabreel <gabrielyury441@gmail.com>
2024-10-02 12:25:55 -0300

    refactor: modelDto receiving a string instead the brand

--- src/main/java/com/tcc5/car_price_compare/domain/vehicle/dto/ModelDTO.java
+++ src/main/java/com/tcc5/car_price_compare/domain/vehicle/dto/ModelDTO.java
@@ -1,8 +1,6 @@
 package com.tcc5.car_price_compare.domain.vehicle.dto;
 
-import com.tcc5.car_price_compare.domain.vehicle.Brand;
-
 import java.util.UUID;
 
-public record ModelDTO(UUID id, String name, String urlPathName, Brand brand) {
+public record ModelDTO(UUID id, String name, String urlPathName, String brand) {
 }
