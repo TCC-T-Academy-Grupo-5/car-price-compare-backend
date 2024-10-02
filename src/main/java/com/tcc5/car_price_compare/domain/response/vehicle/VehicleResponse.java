b2eeeb36e6bbdb2e8df7ba64cac44ce1b1f0cf5c
kingabreel <gabrielyury441@gmail.com>
2024-10-01 21:43:10 -0300

    refactor: VehicleResponse file renamed

--- src/main/java/com/tcc5/car_price_compare/domain/response/vehicle/VehicleResponse.java
+++
@@ -1,7 +0,0 @@
-package com.tcc5.car_price_compare.domain.response.vehicle;
-
-
-import java.util.UUID;
-
-public record VehicleResponse (UUID id, String model, String brand, double fipe_price, String type, String year) {
-}
