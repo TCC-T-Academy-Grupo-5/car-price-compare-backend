e277231f186272c1e6b1b67a8340d4db11dbeb1a
kingabreel <gabrielyury441@gmail.com>
2024-10-02 12:25:02 -0300

    refactor: added where id = id

--- src/main/java/com/tcc5/car_price_compare/repositories/vehicle/BrandRepository.java
+++ src/main/java/com/tcc5/car_price_compare/repositories/vehicle/BrandRepository.java
@@ -4,6 +4,7 @@ import com.tcc5.car_price_compare.domain.vehicle.Brand;
 import com.tcc5.car_price_compare.domain.vehicle.dto.BrandDTO;
 import com.tcc5.car_price_compare.domain.vehicle.dto.VehicleDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,12 +12,13 @@ import java.util.List;
 import java.util.UUID;
 import java.util.Optional;
 
-public interface BrandRepository extends JpaRepository<Brand, UUID> {
+public interface BrandRepository extends JpaRepository<Brand, UUID>, JpaSpecificationExecutor<Brand> {
     Optional<Brand> findByName(String name);
 
     @Query(
             "SELECT new com.tcc5.car_price_compare.domain.vehicle.dto.BrandDTO(b.id, b.name, b.urlPathName, b.vehicleType) " +
-                    "FROM Brand b "
+                    "FROM Brand b " +
+                    "WHERE b.id = :id"
     )
     Optional<BrandDTO> findBrandDTOById(@Param("id") UUID id);
 
