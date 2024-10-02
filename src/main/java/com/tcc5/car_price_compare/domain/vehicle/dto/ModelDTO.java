29e6d11e9e52cc85cb5057e2d6aa180f1aa3f124
kingabreel <gabrielyury441@gmail.com>
2024-10-01 22:08:54 -0300

    refactor: modelDTO converted into addModelDTO

--- src/main/java/com/tcc5/car_price_compare/domain/vehicle/dto/ModelDTO.java
+++ src/main/java/com/tcc5/car_price_compare/domain/vehicle/dto/ModelDTO.java
@@ -1,4 +1,8 @@
 package com.tcc5.car_price_compare.domain.vehicle.dto;
 
-public record ModelDTO(String name, String urlPathName, String brandName) {
+import com.tcc5.car_price_compare.domain.vehicle.Brand;
+
+import java.util.UUID;
+
+public record ModelDTO(UUID id, String name, String urlPathName, Brand brand) {
 }
