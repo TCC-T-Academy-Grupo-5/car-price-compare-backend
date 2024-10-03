bda349d002ca2c2d1d85e89cedbbd8e662e56479
kingabreel <gabrielyury441@gmail.com>
2024-10-03 08:38:56 -0300

    feat: queries for top years, brands, models and vehicles, stats response with parameter name

--- src/main/java/com/tcc5/car_price_compare/domain/response/statistic/StatisticResponseDto.java
+++ src/main/java/com/tcc5/car_price_compare/domain/response/statistic/StatisticResponseDto.java
@@ -2,5 +2,5 @@ package com.tcc5.car_price_compare.domain.response.statistic;
 
 import java.util.UUID;
 
-public record StatisticResponseDto(UUID entityId, Long count) {
+public record StatisticResponseDto(UUID entityId, String name, Long count) {
 }
