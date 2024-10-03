c3307dfe8f8ec1bea02cb7dafe7e53cca95fecbf
kingabreel <gabrielyury441@gmail.com>
2024-10-03 00:47:39 -0300

    feat: get brand, vehicles and models from statistic

--- src/main/java/com/tcc5/car_price_compare/services/StatisticService.java
+++ src/main/java/com/tcc5/car_price_compare/services/StatisticService.java
@@ -1,11 +1,13 @@
 package com.tcc5.car_price_compare.services;
 
+import com.tcc5.car_price_compare.domain.response.statistic.StatisticResponseDto;
 import com.tcc5.car_price_compare.domain.statistic.Statistic;
 import com.tcc5.car_price_compare.domain.statistic.enums.EntityType;
 import com.tcc5.car_price_compare.repositories.StatisticRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -22,4 +24,15 @@ public class StatisticService {
         statisticRepository.save(statistic);
     }
 
+    public List<StatisticResponseDto> getVehicles() {
+        return statisticRepository.findTop10ByEntityType(EntityType.VEHICLE);
+    }
+
+    public List<StatisticResponseDto> getBrands() {
+        return statisticRepository.findTop10ByEntityType(EntityType.BRAND);
+    }
+
+    public List<StatisticResponseDto> getModels() {
+        return statisticRepository.findTop10ByEntityType(EntityType.MODEL);
+    }
 }
