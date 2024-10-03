ffb030534e7f2d3a7e68aeec6f1c2b9a932d90ae
Paulo Henrique <ph.1928@hotmail.com>
2024-10-03 11:30:07 -0300

    refactor: FavoriteRepository now also extends JpaSpecificationExecutor

--- src/main/java/com/tcc5/car_price_compare/repositories/FavoriteRepository.java
+++ src/main/java/com/tcc5/car_price_compare/repositories/FavoriteRepository.java
@@ -2,8 +2,9 @@ package com.tcc5.car_price_compare.repositories;
 
 import com.tcc5.car_price_compare.domain.user.features.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface FavoriteRepository extends JpaRepository<Favorite, UUID> {
+public interface FavoriteRepository extends JpaRepository<Favorite, UUID>, JpaSpecificationExecutor<Favorite> {
 }
