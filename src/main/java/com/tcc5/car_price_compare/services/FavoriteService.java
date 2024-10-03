ada1f8bf2f61fca6938a71a0c2b914facd1a6877
Paulo Henrique <ph.1928@hotmail.com>
2024-10-03 11:31:54 -0300

    feat: add getFavorites to FavoriteController

--- src/main/java/com/tcc5/car_price_compare/services/FavoriteService.java
+++ src/main/java/com/tcc5/car_price_compare/services/FavoriteService.java
@@ -1,7 +1,15 @@
 package com.tcc5.car_price_compare.services;
 
+import com.tcc5.car_price_compare.domain.response.user.FavoriteResponseDTO;
+import com.tcc5.car_price_compare.domain.user.User;
 import com.tcc5.car_price_compare.domain.user.features.Favorite;
 import com.tcc5.car_price_compare.repositories.FavoriteRepository;
+import com.tcc5.car_price_compare.specifications.FavoriteSpecification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,8 +17,23 @@ public class FavoriteService {
 
     private final FavoriteRepository favoriteRepository;
 
-    public FavoriteService(FavoriteRepository favoriteRepository) {
+    private final ConversionService conversionService;
+
+    public FavoriteService(FavoriteRepository favoriteRepository, ConversionService conversionService) {
         this.favoriteRepository = favoriteRepository;
+        this.conversionService = conversionService;
+    }
+
+    public Page<FavoriteResponseDTO> getFavorites(Integer pageNumber, Integer pageSize, Integer vehicleType) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Specification<Favorite> spec = Specification
+                .where(FavoriteSpecification.hasVehicleType(vehicleType))
+                .and(FavoriteSpecification.hasUser(currentUser));
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+
+        return this.favoriteRepository.findAll(spec, pageable).map(this.conversionService::convertToFavoriteResponseDTO);
     }
 
     public Favorite save(Favorite favorite) {
