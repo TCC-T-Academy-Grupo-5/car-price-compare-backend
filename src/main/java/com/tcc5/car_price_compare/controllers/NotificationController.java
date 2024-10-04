f8a2dc97e2abf9c9e0c44846875e528ecb374b0f
Paulo Henrique <ph.1928@hotmail.com>
2024-10-04 15:42:10 -0300

    wip: get endpoint for notifications

--- src/main/java/com/tcc5/car_price_compare/controllers/NotificationController.java
+++ src/main/java/com/tcc5/car_price_compare/controllers/NotificationController.java
@@ -6,13 +6,11 @@ import com.tcc5.car_price_compare.domain.user.features.Notification;
 import com.tcc5.car_price_compare.services.ConversionService;
 import com.tcc5.car_price_compare.services.NotificationService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user/notifications")
@@ -25,6 +23,19 @@ public class NotificationController {
         this.notificationService = notificationService;
         this.conversionService = conversionService;
     }
+
+    @GetMapping
+    public ResponseEntity<Page<NotificationResponseDTO>> getNotifications(
+            @RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
+            @RequestParam(value = "notificationStatus", required = false) Integer notificationStatus
+    ) {
+        Page<NotificationResponseDTO> notifications = this.notificationService.findAll(pageNumber, pageSize, notificationStatus)
+                .map(this.conversionService::convertToNotificationResponseDTO);
+
+        return ResponseEntity.status(HttpStatus.OK).body(notifications);
+    }
+
 
     @PostMapping
     public ResponseEntity<NotificationResponseDTO> createNotification(@RequestBody @Valid NotificationRequestDTO notificationRequestDto) {
--- src/main/java/com/tcc5/car_price_compare/services/NotificationService.java
+++ src/main/java/com/tcc5/car_price_compare/services/NotificationService.java
@@ -1,8 +1,13 @@
 package com.tcc5.car_price_compare.services;
 
+import com.tcc5.car_price_compare.domain.user.User;
 import com.tcc5.car_price_compare.domain.user.features.Notification;
 import com.tcc5.car_price_compare.repositories.NotificationRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class NotificationService {
@@ -15,5 +20,13 @@ public class NotificationService {
 
     public Notification save(Notification notification) {
         return this.notificationRepository.save(notification);
+    }
+
+    public Page<Notification> findAll(Integer pageNumber, Integer pageSize, Integer notificationStatus) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+
+
+        return null;
     }
 }
