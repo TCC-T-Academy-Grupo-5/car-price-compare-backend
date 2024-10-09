20a58a89fb4fa658ad493d5d37c45d4949bfeb02
Paulo Henrique Meneses <124916478+paulohenrique-gh@users.noreply.github.com>
2024-10-08 21:41:51 -0300

    Feature/notifications crud (#25)
    * feat: statistic entity
    
    * feat: statistic repository
    
    * feat: statistic service with increment method
    
    * feat: convert vehicle to vehicleResponsedto
    
    * feat: statistics, vehicle conversion changed to conversion service and getVehicleById
    
    * feat: search vehicle by id
    
    * feat: statistic response
    
    * feat: search top 10 entities in statistic
    
    * feat: get brand, vehicles and models from statistic
    
    * feat: endpoints at statistic controller
    
    * feat: year endpoint and response statistic for vehicle
    
    * feat: queries for top years, brands, models and vehicles, stats response with parameter name
    
    * refactor: repository methods name changed
    
    * wip: get endpoint for notifications
    
    * feat: implement NotificationController#getNotifications
    
    * feat: implement getNotificationById
    
    * feat: implement updateNotification
    
    * feat: implement deleteNotification
    
    * docs: add notification endpoints to readme
    
    ---------
    
    Co-authored-by: kingabreel <gabrielyury441@gmail.com>

--- src/main/java/com/tcc5/car_price_compare/controllers/NotificationController.java
+++ src/main/java/com/tcc5/car_price_compare/controllers/NotificationController.java
@@ -6,13 +6,13 @@ import com.tcc5.car_price_compare.domain.user.features.Notification;
 import com.tcc5.car_price_compare.services.ConversionService;
 import com.tcc5.car_price_compare.services.NotificationService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user/notifications")
@@ -26,13 +26,48 @@ public class NotificationController {
         this.conversionService = conversionService;
     }
 
+    @GetMapping
+    public ResponseEntity<Page<NotificationResponseDTO>> getNotifications(
+            @RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
+            @RequestParam(value = "status", required = false) Integer status
+    ) {
+        Page<NotificationResponseDTO> notifications = this.notificationService.findAll(pageNumber, pageSize, status)
+                .map(this.conversionService::convertToNotificationResponseDTO);
+
+        return ResponseEntity.status(HttpStatus.OK).body(notifications);
+    }
+
+    @GetMapping("/{notificationId}")
+    public ResponseEntity<NotificationResponseDTO> getNotificationById(@PathVariable UUID notificationId) {
+        NotificationResponseDTO notificationResponseDTO = this.conversionService
+                .convertToNotificationResponseDTO(this.notificationService.findById(notificationId));
+        return ResponseEntity.status(HttpStatus.OK).body(notificationResponseDTO);
+    }
+
+
     @PostMapping
     public ResponseEntity<NotificationResponseDTO> createNotification(@RequestBody @Valid NotificationRequestDTO notificationRequestDto) {
         Notification notification = this.conversionService.convertToNotificationEntity(notificationRequestDto);
         NotificationResponseDTO notificationResponseDTO = this.conversionService
                 .convertToNotificationResponseDTO(this.notificationService.save(notification));
 
-        // TODO: change status to created with the uri of the new notification when GET notifications is implemented
         return ResponseEntity.status(HttpStatus.CREATED).body(notificationResponseDTO);
+    }
+
+    @PutMapping("/{notificationId}")
+    public ResponseEntity<NotificationResponseDTO> updateFavorite(@PathVariable UUID notificationId,
+            @RequestBody @Valid NotificationRequestDTO notificationRequestDto) {
+        Notification update = this.conversionService.convertToNotificationEntity(notificationRequestDto);
+        Notification updatedNotification = this.notificationService.update(notificationId, update);
+        NotificationResponseDTO notificationResponseDTO = this.conversionService.convertToNotificationResponseDTO(updatedNotification);
+
+        return ResponseEntity.status(HttpStatus.OK).body(notificationResponseDTO);
+    }
+
+    @DeleteMapping("/{notificationId}")
+    public ResponseEntity<?> deleteNotification(@PathVariable UUID notificationId) {
+        this.notificationService.delete(notificationId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/tcc5/car_price_compare/services/NotificationService.java
+++ src/main/java/com/tcc5/car_price_compare/services/NotificationService.java
@@ -1,8 +1,18 @@
 package com.tcc5.car_price_compare.services;
 
+import com.tcc5.car_price_compare.domain.user.User;
+import com.tcc5.car_price_compare.domain.user.exceptions.NotificationNotFoundException;
 import com.tcc5.car_price_compare.domain.user.features.Notification;
 import com.tcc5.car_price_compare.repositories.NotificationRepository;
+import com.tcc5.car_price_compare.specifications.NotificationSpecification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class NotificationService {
@@ -13,7 +23,44 @@ public class NotificationService {
        this.notificationRepository = notificationRepository;
     }
 
+    public Page<Notification> findAll(Integer pageNumber, Integer pageSize, Integer notificationStatus) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Specification<Notification> spec = Specification
+                .where(NotificationSpecification.hasStatus(notificationStatus))
+                .and(NotificationSpecification.hasUser(currentUser));
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+
+        return this.notificationRepository.findAll(spec, pageable);
+    }
+
+    public Notification findById(UUID notificationId) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return this.notificationRepository
+                .findByIdAndUser(notificationId, currentUser)
+                .orElseThrow(() -> new NotificationNotFoundException(notificationId));
+    }
+
     public Notification save(Notification notification) {
         return this.notificationRepository.save(notification);
+    }
+
+    public Notification update(UUID notificationId, Notification notification) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return this.notificationRepository.findByIdAndUser(notificationId, currentUser)
+                .map(oldNotification -> {
+                    oldNotification.setNotificationType(notification.getNotificationType());
+                    oldNotification.setCurrentFipePrice(notification.getCurrentFipePrice());
+                    oldNotification.setVehicle(notification.getVehicle());
+                    return this.notificationRepository.save(oldNotification);
+                })
+                .orElseThrow(() -> new NotificationNotFoundException(notificationId));
+    }
+
+    public void delete(UUID notificationId) {
+        this.notificationRepository.delete(this.findById(notificationId));
     }
 }
