5c5c65462feebee3c738349a7390e5ff00c4d26b
Paulo Henrique <ph.1928@hotmail.com>
2024-10-10 15:43:38 -0300

    feat: create ModelCategory enum

---
+++ src/main/java/com/tcc5/car_price_compare/domain/vehicle/enums/ModelCategory.java
@@ -0,0 +1,68 @@
+package com.tcc5.car_price_compare.domain.vehicle.enums;
+
+public enum ModelCategory {
+    COMPACT_SEDAN("Sedã compacto"),
+    SUBCOMPACT_HATCH("Hatch subcompacto"),
+    MEDIUM_CONVERTIBLE("Conversível médio"),
+    LARGE_SEDAN("Sedã grande"),
+    LARGE_SUV("SUV grande"),
+    COMPACT_SPORTS("Esportivo compacto"),
+    COMPACT_VAN("Furgão compacto"),
+    COMPACT_SUV("SUV compacto"),
+    MEDIUM_VAN("Van média"),
+    URBAN_TRUCK("Caminhão urbano"),
+    BUGGY("Buggy"),
+    MEDIUM_FAMILY("Familiar médio"),
+    MEDIUM_HATCH("Hatch médio"),
+    LARGE_PICKUP("Picape grande"),
+    MEDIUM_SUV("SUV médio"),
+    LARGE_FAMILY("Familiar grande"),
+    COMPACT_ADVENTURER("Aventureiro compacto"),
+    COMPACT_HATCH("Hatch compacto"),
+    COMPACT_FAMILY("Familiar compacto"),
+    LARGE_VAN("Van grande"),
+    ELECTRIC("Elétrico"),
+    MEDIUM_PICKUP("Picape média"),
+    LARGE_CONVERTIBLE("Conversível grande"),
+    JEEP("Jipe"),
+    MEDIUM_SPORTS("Esportivo médio"),
+    MEDIUM_SEDAN("Sedã médio"),
+    MEDIUM_FURGON("Furgão médio"),
+    COMPACT_CONVERTIBLE("Conversível compacto"),
+    LARGE_SPORTS("Esportivo grande"),
+    COMPACT_PICKUP("Picape compacta"),
+    TOURING("Touring"),
+    STREET("Street"),
+    SPORT("Sport"),
+    CUSTOM("Custom"),
+    QUAD_UTV("Quadriciclo UTV"),
+    NAKED("Naked"),
+    CUB("Cub"),
+    CARGO_TRICYCLE("Triciclo de Carga"),
+    ATV_QUAD("Quadriciclo (ATV)"),
+    MOPED("Mobilete"),
+    TRICYCLE("Triciclo"),
+    TRAIL("Trail"),
+    SCOOTER("Scooter");
+
+    private final String portugueseTranslation;
+
+    ModelCategory(String portugueseTranslation) {
+        this.portugueseTranslation = portugueseTranslation;
+    }
+
+    public String getPortugueseTranslation() {
+        return portugueseTranslation;
+    }
+
+    public static ModelCategory fromString(String value) {
+
+        for (ModelCategory category : ModelCategory.values()) {
+            if (category.getPortugueseTranslation().equals(value)) {
+                return category;
+            }
+        }
+
+        throw new RuntimeException("Value not found: " + value);
+    }
+}
--- src/main/java/com/tcc5/car_price_compare/infra/fipedata/converter/ModelJsonDtoToModelConverter.java
+++ src/main/java/com/tcc5/car_price_compare/infra/fipedata/converter/ModelJsonDtoToModelConverter.java
@@ -2,6 +2,7 @@ package com.tcc5.car_price_compare.infra.fipedata.converter;
 
 import com.tcc5.car_price_compare.domain.vehicle.Brand;
 import com.tcc5.car_price_compare.domain.vehicle.Model;
+import com.tcc5.car_price_compare.domain.vehicle.enums.ModelCategory;
 import com.tcc5.car_price_compare.infra.fipedata.dto.ModelJsonDto;
 import com.tcc5.car_price_compare.repositories.vehicle.BrandRepository;
 import org.springframework.core.convert.converter.Converter;
@@ -28,6 +29,7 @@ public class ModelJsonDtoToModelConverter implements Converter<ModelJsonDto, Mod
         model.setId(UUID.fromString(source.id()));
         model.setName(source.name());
         model.setUrlPathName(source.url_path_name());
+        model.setCategory(ModelCategory.fromString(source.categoryString()));
         model.setBrand(brand);
         model.setImageUrl(brand.getImageUrl());
 
