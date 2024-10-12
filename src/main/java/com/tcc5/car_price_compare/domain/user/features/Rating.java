9f188573274bded714d9f98545f9cfa204510773
kingabreel <gabrielyury441@gmail.com>
2024-10-11 21:41:16 -0300

    refactor: string to uuid at rating id

--- src/main/java/com/tcc5/car_price_compare/domain/user/features/Rating.java
+++ src/main/java/com/tcc5/car_price_compare/domain/user/features/Rating.java
@@ -6,6 +6,8 @@ import com.tcc5.car_price_compare.domain.vehicle.Vehicle;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.UUID;
+
 @Table(name = "rating")
 @Entity(name = "rating")
 @Getter
@@ -16,7 +18,7 @@ import lombok.*;
 public class Rating {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    private UUID id;
     private String message;
     private int rate;
 
