6720d1eb8d63983ed789334059a69e7b3f02e502
Paulo Henrique <ph.1928@hotmail.com>
2024-10-13 16:28:10 -0300

    chore: GeneralConfig class

---
+++ src/main/java/com/tcc5/car_price_compare/config/GeneralConfig.java
@@ -0,0 +1,14 @@
+package com.tcc5.car_price_compare.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class GeneralConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+}
