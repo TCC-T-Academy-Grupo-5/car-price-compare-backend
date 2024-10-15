99f358ae6c74a8f4a086f91d09e7cf0a51456997
alex cesar <thealexcesar@gmail.com>
2024-10-15 14:10:59 -0300

    ✨feat: headers para endpoint -> vehicle

--- src/main/java/com/tcc5/car_price_compare/controllers/VehicleController.java
+++ src/main/java/com/tcc5/car_price_compare/controllers/VehicleController.java
@@ -6,9 +6,13 @@ import com.tcc5.car_price_compare.domain.vehicle.Brand;
 import com.tcc5.car_price_compare.domain.vehicle.Model;
 import com.tcc5.car_price_compare.domain.vehicle.dto.*;
 import com.tcc5.car_price_compare.services.VehicleService;
+import com.tcc5.car_price_compare.shared.utils.PaginationHeaders;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,24 +24,31 @@ import java.util.UUID;
 @RequestMapping("vehicle")
 public class VehicleController {
 
-    @Autowired
-    private VehicleService service;
+    private final VehicleService service;
+
+    public VehicleController(VehicleService service) {
+        this.service = service;
+    }
 
     @GetMapping
-    public ResponseEntity<Page<VehicleResponseDTO>> getVehicles(
-            @RequestParam(value = "pageNumber", defaultValue = "0") Integer pageNumber,
+    public ResponseEntity<List<VehicleResponseDTO>> getVehicles(
+            @RequestParam(value = "pageNumber", defaultValue = "1") Integer pageNumber,
             @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
             @RequestParam(value = "model", required = false) String model,
             @RequestParam(value = "brand", required = false) String brand,
             @RequestParam(value = "fipePrice", required = false) Double fipePrice,
             @RequestParam(value = "type", required = false) Integer type,
             @RequestParam(value = "year", required = false) String year
-            ){
+    ) {
+        pageNumber = Math.max(1, pageNumber);
+        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize);
+        Page<VehicleResponseDTO> vehicles = service.getVehicles(model, brand, fipePrice, type, year, pageable);
+        HttpHeaders headers = PaginationHeaders.createPaginationHeaders(vehicles);
 
-        return ResponseEntity.status(HttpStatus.OK).body(service.getVehicles(pageNumber, pageSize, model, brand, fipePrice, type, year));
+        return ResponseEntity.ok().headers(headers).body(vehicles.getContent());
     }
 
-    @GetMapping("/{id}")
+        @GetMapping("/{id}")
     public ResponseEntity<VehicleResponseDTO> getVehicle(@PathVariable String id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.getVehicleById(id));
     }
