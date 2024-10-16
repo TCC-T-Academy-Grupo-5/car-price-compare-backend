fdf80ec1b60ceb2b55919256522289a0185ec2d9
Paulo Henrique <ph.1928@hotmail.com>
2024-10-16 11:32:55 -0300

    test: change AuthorizationServiceTest class annotation

--- src/test/java/com/tcc5/car_price_compare/auth/AuthorizationServiceTest.java
+++ src/test/java/com/tcc5/car_price_compare/auth/AuthorizationServiceTest.java
@@ -3,35 +3,39 @@ package com.tcc5.car_price_compare.auth;
 import com.tcc5.car_price_compare.repositories.UserRepository;
 import com.tcc5.car_price_compare.services.AuthorizationService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-@SpringBootTest
-public class AuuthorizationServiceTest {
+@ExtendWith(MockitoExtension.class)
+public class AuthorizationServiceTest {
 
-    @Autowired
+    @InjectMocks
     private AuthorizationService authorizationService;
 
-    @MockBean
+    @Mock
     private UserRepository userRepository;
 
     @Test
     public void testLoadUserByUserNameSuccess() {
         String email = "user@example.com";
         UserDetails userDetails = Mockito.mock(UserDetails.class);
-        given(userRepository.findByEmail(email)).willReturn(userDetails);
+        given(this.userRepository.findByEmail(email)).willReturn(userDetails);
 
-        UserDetails result = authorizationService.loadUserByUsername(email);
+        UserDetails result = this.authorizationService.loadUserByUsername(email);
 
         assertNotNull(result);
         assertEquals(userDetails, result);
+        verify(this.userRepository, times(1)).findByEmail(email);
     }
 
     @Test
