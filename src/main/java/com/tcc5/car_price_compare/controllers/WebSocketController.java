7a758a0d3172a7b993ca82cd3febdfa34be7be12
kingabreel <gabrielyury441@gmail.com>
2024-10-18 17:53:50 -0300

    feat: message converter configuration and payload

--- src/main/java/com/tcc5/car_price_compare/controllers/WebSocketController.java
+++ src/main/java/com/tcc5/car_price_compare/controllers/WebSocketController.java
@@ -2,6 +2,7 @@ package com.tcc5.car_price_compare.controllers;
 
 import com.tcc5.car_price_compare.domain.response.user.NotificationResponseDTO;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
@@ -10,7 +11,7 @@ public class WebSocketController {
 
     @MessageMapping("/notify")
     @SendTo("/topic/notification")
-    public NotificationResponseDTO sendNotification(NotificationResponseDTO notificationResponseDTO){
+    public NotificationResponseDTO sendNotification(@Payload NotificationResponseDTO notificationResponseDTO){
         return notificationResponseDTO;
     }
 }
