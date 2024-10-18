a17350cf68fce0fb007101a0cbf7a7c2781a6294
Paulo Henrique Meneses <124916478+paulohenrique-gh@users.noreply.github.com>
2024-10-18 12:55:04 -0300

    Melhoria de performance ao retornar marcas (#40)
    * performance: improve performance when querying for brands
    
    adjust BrandToBrandDTOConverter
    
    * chore: remove unused imports

--- src/main/java/com/tcc5/car_price_compare/domain/vehicle/converters/BrandToBrandDTOConverter.java
+++ src/main/java/com/tcc5/car_price_compare/domain/vehicle/converters/BrandToBrandDTOConverter.java
@@ -2,23 +2,20 @@ package com.tcc5.car_price_compare.domain.vehicle.converters;
 
 import com.tcc5.car_price_compare.domain.vehicle.Brand;
 import com.tcc5.car_price_compare.domain.vehicle.dto.BrandDTO;
-import com.tcc5.car_price_compare.domain.vehicle.exceptions.BrandNotFoundException;
-import com.tcc5.car_price_compare.repositories.vehicle.BrandRepository;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BrandToBrandDTOConverter implements Converter<Brand, BrandDTO> {
 
-    private final BrandRepository brandRepository;
-
-    public BrandToBrandDTOConverter(BrandRepository brandRepository) {
-        this.brandRepository = brandRepository;
-    }
-
     @Override
     public BrandDTO convert(Brand source) {
-        return this.brandRepository.findBrandDTOById(source.getId())
-                .orElseThrow(() -> new BrandNotFoundException(source.getId()));
+        return new BrandDTO(
+                source.getId(),
+                source.getName(),
+                source.getUrlPathName(),
+                source.getImageUrl(),
+                source.getVehicleType()
+        );
     }
 }
